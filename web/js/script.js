0fa2efbdea09792b3d5fffc3f62dbda5dc4fd9c8
user <user@10.212.12.38>
2014-12-19 05:14:03 +0800

    add calendar

---
+++ web/js/script.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function() {
+    $("#departing").datepicker();
+    $("#returning").datepicker();
+    $("button").click(function() {
+    	var selected = $("#dropdown option:selected").text();
+        var departing = $("#departing").val();
+        var returning = $("#returning").val();
+        if (departing === "" || returning === "") {
+			alert("Please select DATE");
+        } 
+    });
+});
+
